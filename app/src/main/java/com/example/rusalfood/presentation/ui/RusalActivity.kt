1498deb3ffae7d427347020fa6cfc363e8f3abd7
Aldayar <luuuqra@gmail.com>
2022-02-05 19:15:13 +0600

    Initial commit

---
+++ app/src/main/java/com/example/rusalfood/presentation/ui/RusalActivity.kt
@@ -0,0 +1,12 @@
+package com.example.rusalfood.presentation.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.rusalfood.R
+
+class RusalActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_rusalfood)
+    }
+}
