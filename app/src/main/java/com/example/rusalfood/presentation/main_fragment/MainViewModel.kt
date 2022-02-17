8700903daa2c15682f53759df2c9e4bc5580a244
Aldayar <luuuqra@gmail.com>
2022-02-18 01:39:20 +0600

    Skeletons

--- app/src/main/java/com/example/rusalfood/presentation/main_fragment/MainViewModel.kt
+++ app/src/main/java/com/example/rusalfood/presentation/main_fragment/MainViewModel.kt
@@ -4,8 +4,10 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.rusalfood.domain.models.Place
+import com.example.rusalfood.domain.models.Resource
 import com.example.rusalfood.domain.usecases.DisplayAllPlacesUseCase
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 class MainViewModel(
@@ -16,14 +18,18 @@ class MainViewModel(
         displayAllPlaces()
     }
 
-    private val _listPlaces = MutableLiveData<List<Place>>()
-    val listPlaces: MutableLiveData<List<Place>> = _listPlaces
+    private val _listPlaces = MutableLiveData<Resource<List<Place>>>()
+    val listPlaces: MutableLiveData<Resource<List<Place>>> = _listPlaces
 
     private val _isAuthorized = MutableLiveData<Boolean>()
     val isAuthorized = _isAuthorized
 
     private fun displayAllPlaces() = viewModelScope.launch(Dispatchers.IO) {
+        listPlaces.postValue(Resource.Loading())
+        delay(2000) // skeletons ;)
         val places = displayAllPlacesUseCase.execute()
-        listPlaces.postValue(places)
+        listPlaces.postValue(Resource.Success(places))
     }
+
+
 }
