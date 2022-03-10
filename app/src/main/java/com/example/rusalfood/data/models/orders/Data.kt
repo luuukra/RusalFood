b3707b68d0c5af2e143af3e6973afa0cfa92fe5b
Ruslan Khujaniyazov <rkhuzhaniyazov@mail.ru>
2022-03-10 15:10:31 +0600

    last fixes

--- app/src/main/java/com/example/rusalfood/data/models/orders/Data.kt
+++ app/src/main/java/com/example/rusalfood/data/models/orders/Data.kt
@@ -1,17 +1,5 @@
 package com.example.rusalfood.data.models.orders
 
-
-import com.google.gson.annotations.SerializedName
-
 data class Data(
-    @SerializedName("order_detail")
-    val orderDetail: List<OrderDetail>,
-    @SerializedName("order_id")
-    val orderId: Int,
-    @SerializedName("order_status")
-    val orderStatus: String,
-    val restaurant: Restaurant,
-    val total: Int,
-    @SerializedName("user_id")
-    val userId: Int
+    val order: List<Order>
 )
